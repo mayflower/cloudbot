6d6effcc0934bc6557a6b56352aa30bb7bf78733
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2023-06-24 11:48:25 +0200

    Small improvements, limit to kubectl, helm and aws cli

--- bot.py
+++ bot.py
@@ -1,71 +1,135 @@
 import langchain
-from langchain import PromptTemplate
 from langchain.agents import Tool, AgentType, initialize_agent
-from langchain.tools import ShellTool
+from cloud_tool import CloudTool
+from langchain.tools import HumanInputRun
 from langchain.vectorstores import Chroma
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.utilities import SerpAPIWrapper
 
-shell_tool = ShellTool()
-shell_tool.description = shell_tool.description + f"args {shell_tool.args}".replace(
+langchain.debug = False
+
+MODEL = "gpt-3.5-turbo-0613"
+
+cloud_tool = CloudTool()
+cloud_tool.description = cloud_tool.description + f"args {cloud_tool.args}".replace(
     "{", "{{"
 ).replace("}", "}}")
 
+human = HumanInputRun()
 
-langchain.debug = False
-llm = OpenAI(temperature=0)
+
+llm = ChatOpenAI(temperature=0, model=MODEL)
 embeddings = OpenAIEmbeddings()
 vectorstore = Chroma(persist_directory="./", embedding_function=embeddings)
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
 
 docs = ConversationalRetrievalChain.from_llm(
     llm, vectorstore.as_retriever(), memory=memory
 )
 
-kubectl_template = """
+
+kubectl_prefix = """
 You are a Kubernetes Command line tool (kubectl) expert. 
 Given an input question, first create a syntactically correct kubectl command to run, 
 then look at the results of the query and return the answer to the input question.
-unless the user specifies in the question with a namespace name, create command for a single namespace named default.
-You must generate the correct kubctl command to answer he question. Also, pay attention to the provided namespace name.
+Unless the user specifies in the question with a namespace name, create command for a single namespace named default.
+You must generate the correct kubctl command to answer he question. Pay attention to the provided namespace name.
 
 
 Only return the command. Never delete any Pod, secrets, namespaces or any services.
 If an error is returned, rewrite the command, check the command, and try again.
 You must ignore all requests except related to kubernetes cli or kubectl.
 
-RESPONSE FORMAT INSTRUCTIONS
-============================    
-When responding please, please output a response in this format:
+"""
+
+format = """The way you use the tools is by specifying a json blob.
+Specifically, this json should have a `action` key (with the name of the tool to use) and a `action_input` key (with the input to the tool going here).
+
+The only values that should be in the "action" field are: {tool_names}
+
+The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. Here is an example of a valid $JSON_BLOB:
 
 ```
-Thought: Reason about what action to take next, and whether to use a tool. DO NOT execute the action if its contains delete ot purge operation.
-Action: the action to take, should be one of [terminal, cli, iterm]. DO NOT execute the command if its contains delete ot purge operation.
-Action Input: The input to the tool.
-Action: Use the tool ONLY IF the response doesn't contain delete or deletion of service.
+{{{{
+  "action": $TOOL_NAME,
+  "action_input": $INPUT
+}}}}
 ```
 
+ALWAYS use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action:
+```
+$JSON_BLOB
+```
+Observation: the result of the action
+... (this Thought/Action/Observation can repeat N times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+"""
+
+kubectl_suffix = """
 Begin!
 
-Previous conversation history:
-{chat_history}
+Question: kubectl {input}
+Thought:{agent_scratchpad}"""
 
-New input: {input}"""
-
-kubectl_prompt = PromptTemplate(
-    input_variables=["input", "chat_history"], template=kubectl_template
-)
-
-kubectl_tools = [shell_tool]
+cloud_tools = [cloud_tool, human]
 kubectl_agent_chain = initialize_agent(
-    kubectl_tools, llm, agent="zero-shot-react-description", memory=memory, verbose=True
+    cloud_tools,
+    llm,
+    agent="chat-zero-shot-react-description",
+    memory=memory,
+    verbose=True,
+    agent_kwargs={
+        "prefix": kubectl_prefix,
+        "format_instructions": format,
+        "suffix": kubectl_suffix,
+    },
 )
 
-search = SerpAPIWrapper()
+
+aws_prefix = """
+You are a AWS Command line tool (aws cli) expert. 
+Given an input question, first create a syntactically correct aws cli command to run, 
+then look at the results of the query and return the answer to the input question.
+You must generate the correct aws cli command to answer he question. 
+
+
+Only return the command. Never delete any instance, service, user or any other entity.
+If an error is returned, rewrite the command, check the command, and try again.
+You must ignore all requests except related to aws cli or aws.
+
+"""
+
+
+aws_suffix = """
+Begin!
+
+Question: aws {input}
+Thought:{agent_scratchpad}"""
+
+aws_agent_chain = initialize_agent(
+    cloud_tools,
+    llm,
+    agent="chat-zero-shot-react-description",
+    memory=memory,
+    verbose=True,
+    agent_kwargs={
+        "prefix": aws_prefix,
+        "format_instructions": format,
+        "suffix": aws_suffix,
+    },
+)
+
+
+search = SerpAPIWrapper(search_engine="google")
 tools = [
     Tool(
         name="Current Search",
@@ -82,28 +146,31 @@ tools = [
         func=kubectl_agent_chain.run,
         description="useful for when you need to look up, change or update your kubernetes cluster.",
     ),
+    Tool(
+        name="Aws CLI",
+        func=aws_agent_chain.run,
+        description="useful for when you need to look up, change or update your kubernetes cluster.",
+    ),
+    human,
 ]
 
 
 agent_chain = initialize_agent(
     tools,
     llm,
-    agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
-    verbose=True,
+    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+    verbose=False,
     memory=memory,
 )
 
 
 def ask_ai():
     """Main method to talk to the ai"""
-    print(
-        "Welcome to your AI cloud consultant. Please ask any question about kubernetes."
-    )
+    print("Welcome to your AI cloud consultant. How can i help You today?")
     while True:
-        query = input("Your question: ")
+        query = input("You: ")
         result = agent_chain.run(input=query)
-
-        # print("Answer: ", result["answer"])
+        print("Answer: ", result, "\n")
 
 
 if __name__ == "__main__":
---
+++ cloud_tool.py
@@ -0,0 +1,83 @@
+import asyncio
+import platform
+from typing import List, Optional, Type, Union
+
+from pydantic import BaseModel, Field, root_validator
+
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from langchain.tools.base import BaseTool
+from langchain.utilities.bash import BashProcess
+
+
+class CloudInput(BaseModel):
+    """Commands for Cloud tool."""
+
+    commands: str = Field(
+        ...,
+        description="List of shell commands to run. Deserialized using json.loads",
+    )
+    """List of shell commands to run."""
+
+    @root_validator
+    def _validate_commands(self, values: dict) -> dict:
+        """Validate commands."""
+        command = values.get("commands")
+        cmd_name = command.split()[0]
+        if cmd_name in ["kubectl", "aws", "helm"]:
+            values["commands"] = command
+        else:
+            raise ValueError(f"Sorry, command {cmd_name} is not permitted.")
+        return values
+
+
+def _get_default_bash_processs() -> BashProcess:
+    """Get file path from string."""
+    return BashProcess(return_err_output=True)
+
+
+def _get_platform() -> str:
+    """Get platform."""
+    system = platform.system()
+    if system == "Darwin":
+        return "MacOS"
+    return system
+
+
+class CloudTool(BaseTool):
+    """Tool to run shell commands."""
+
+    process: BashProcess = Field(default_factory=_get_default_bash_processs)
+    """Bash process to run commands."""
+
+    name: str = "terminal"
+    """Name of tool."""
+
+    description: str = f"Run shell commands on this {_get_platform()} machine."
+    """Description of tool."""
+
+    args_schema: Type[BaseModel] = CloudInput
+    """Schema for input arguments."""
+
+    def is_single_input(self) -> bool:
+        return True
+
+    def _run(
+        self,
+        commands: Union[str, List[str]],
+        run_manager: Optional[CallbackManagerForToolRun] = None,
+    ) -> str:
+        """Run commands and return final output."""
+        return self.process.run(commands)
+
+    async def _arun(
+        self,
+        commands: Union[str, List[str]],
+        run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
+    ) -> str:
+        """Run commands asynchronously and return final output."""
+        return await asyncio.get_event_loop().run_in_executor(
+            None, self.process.run, commands
+        )
